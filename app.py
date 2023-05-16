12fbba201227a8713ed76d0de5556b3aab58dda5
BaeZiTa <126735559+BaeZiTa@users.noreply.github.com>
2023-05-16 12:45:45 +0900

    Update app.py

--- app.py
+++ app.py
@@ -6,5 +6,5 @@ st.title("Map")
 df = pd.DataFrame(np.random.randn(500, 2) / [50, 50] + [37.76, -122.4], columns = ['lat', 'lon'])
 st.map(df)
 
-#x = st.slider('Select a value')
-#st.write(x, 'squared is', x * x)
+x = st.slider('Select a value')
+st.write(x, 'squared is', x * x)
